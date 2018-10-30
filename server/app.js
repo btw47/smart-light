2a97d2137e653b6b4db1b6d28285f76c9bef8a96
Bradley Wong <wong.bradley21@gmail.com>
2018-10-29 19:46:41 -0700

    Clean up

--- server/app.js
+++ server/app.js
@@ -1,23 +1,15 @@
-var app = require('http').createServer(handler);
-var io = require('socket.io')(app);
-var url = require('url');
 var fs = require('fs');
+var io = require('socket.io')(app);
+var app = require('http').createServer(handler);
 
 app.listen(5000);
 
-function handler(req, res) {
-	console.log("ITS RUNNING");
-}
-
-io.sockets.on('connection', function(socket) {
+//declare actions to perform on websocket connection
+io.sockets.on('connection', socket => {
+	console.log('Connection made.');
 
 	socket.on('example-ping', data => {
-		console.log("ping");
-
-		var delay = data.duration;
-
-		setTimeout(function() {
-			socket.emit("example-pong");
-		}, delay * 1000);
+		socket.emit('example-pong');
 	})
+
 })
